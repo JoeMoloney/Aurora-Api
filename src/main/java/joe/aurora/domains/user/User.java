be0d6bde0305919cbded58fd73f630d02e0bd926
Joe Moloney <joe.moloney496@gmail.com>
2026-05-28 14:44:23 +0100

    add slf4j logging

--- src/main/java/joe/aurora/domains/user/User.java
+++ src/main/java/joe/aurora/domains/user/User.java
@@ -3,8 +3,10 @@ package joe.aurora.domains.user;
 import java.time.LocalDateTime;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
+@Slf4j
 public class User {
     private Long id;
     private String email;
