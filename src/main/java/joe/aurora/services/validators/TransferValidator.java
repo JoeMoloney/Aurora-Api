edf2a15410337b4a3ce4b8014a50a90788eaa99b
Joe Moloney <joe.moloney496@gmail.com>
2026-05-31 10:26:38 +0100

    add initial transfer validator

---
+++ src/main/java/joe/aurora/services/validators/TransferValidator.java
@@ -0,0 +1,11 @@
+package joe.aurora.services.validators;
+
+import java.math.BigDecimal;
+
+import joe.aurora.domains.wallet.Wallet;
+
+public class TransferValidator {
+    public void validate(Wallet wallet, BigDecimal amount) {
+        throw new UnsupportedOperationException("Function not implemented");
+    } 
+}
