f3dd3ceab978fa7b3e7384e30e3cbb40807b7446
Joe Moloney <joe.moloney496@gmail.com>
2026-05-31 10:35:02 +0100

    add all-arg constructor

--- src/main/java/joe/aurora/dtos/ErrorResponseDTO.java
+++ src/main/java/joe/aurora/dtos/ErrorResponseDTO.java
@@ -2,12 +2,23 @@ package joe.aurora.dtos;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ErrorResponseDTO {
     private LocalDateTime timestamp;
-    private Integer statusCode;
+    private HttpStatus statusCode;
     private String errorReason;
     private String details;
+    private String uriPath;
+
+    public ErrorResponseDTO(LocalDateTime ldt, HttpStatus httpStatus, String reason, String exceptionMessage, String uri) {
+        this.timestamp = ldt;
+        this.statusCode = httpStatus;
+        this.errorReason = reason;
+        this.details = exceptionMessage;
+        this.uriPath = uri;
+    }
 }
