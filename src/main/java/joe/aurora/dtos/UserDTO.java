0808450f65dfca5a2afb1b8289e24e0a0cd3f6ed
Joe Moloney <joe.moloney496@gmail.com>
2026-05-31 10:34:24 +0100

    add userDTO

---
+++ src/main/java/joe/aurora/dtos/UserDTO.java
@@ -0,0 +1,20 @@
+package joe.aurora.dtos;
+
+public class UserDTO {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return this.email;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
