b489147f87e28887ab4c6df3e60f2cf331907d9c
Joe Moloney <joe.moloney496@gmail.com>
2026-05-31 09:52:42 +0100

    add repositories and inheritence

--- src/main/java/joe/aurora/services/serviceimpls/WalletServiceImpl.java
+++ src/main/java/joe/aurora/services/serviceimpls/WalletServiceImpl.java
@@ -1,5 +1,17 @@
 package joe.aurora.services.serviceimpls;
 
-public class WalletServiceImpl {
-    
+import java.math.BigDecimal;
+
+import joe.aurora.repositories.TransactionRepository;
+import joe.aurora.repositories.WalletRepository;
+import joe.aurora.services.WalletService;
+
+public class WalletServiceImpl implements WalletService {
+    private WalletRepository walletRepository;
+    private TransactionRepository transactionRepository;
+    // private TransferValidator transferValidator;
+
+    public void executeTransfer(Long src, Long dest, BigDecimal amount) {
+        throw new UnsupportedOperationException("Function not implemented");
+    }
 }
