13bd996e98d3ba16eb3e277fcf7bbf898632ad22
Joe Moloney <joe.moloney496@gmail.com>
2026-05-31 10:35:48 +0100

    add insufficient funds handling logic

--- src/main/java/joe/aurora/exceptions/GlobalExceptionHandler.java
+++ src/main/java/joe/aurora/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,27 @@
 package joe.aurora.exceptions;
 
-public class GlobalExceptionHandler extends Exception{
-    public GlobalExceptionHandler(String message) {
-        super(message);
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import joe.aurora.dtos.ErrorResponseDTO;
+import reactor.netty.http.server.HttpServerRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends Exception {
+    @ExceptionHandler(InsufficientFundsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInsufficientFunds(InsufficientFundsException ex, HttpServerRequest request) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+            LocalDateTime.now(),
+            HttpStatus.UNPROCESSABLE_ENTITY,
+            HttpStatus.UNPROCESSABLE_ENTITY.getReasonPhrase(),
+            ex.getMessage(),
+            request.uri()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 }
