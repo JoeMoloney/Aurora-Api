7d0fda46673d72f51e6a8e56a58fc4df3f0dc087
Joe Moloney <joe.moloney496@gmail.com>
2026-06-16 17:57:16 +0100

    add validation logic

---
+++ src/main/java/joe/aurora/services/validators/validatorimpls/TransferValidatorImpls.java
@@ -0,0 +1,23 @@
+package joe.aurora.services.validators.validatorimpls;
+
+import java.math.BigDecimal;
+
+import joe.aurora.domains.wallet.Wallet;
+import joe.aurora.exceptions.InappropriateTransferAmountException;
+import joe.aurora.exceptions.InsufficientFundsException;
+import joe.aurora.services.validators.TransferValidator;
+
+public class TransferValidatorImpls implements TransferValidator {
+
+    // 0 = a == b
+    // 1 = a > b
+    // -1 = a < b
+    public void validate(Wallet wallet, BigDecimal amount) {
+        if(amount.compareTo(BigDecimal.ZERO) == 0) {
+            throw new InappropriateTransferAmountException("Transfer Must Be More Than 0");
+        }
+        else if(amount.compareTo(wallet.getBalance()) == -1) {
+            throw new InsufficientFundsException("Insufficient Funds For Transfer");
+        }
+    }
+}
