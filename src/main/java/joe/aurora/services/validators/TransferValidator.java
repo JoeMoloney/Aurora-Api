d098de3ae3bf96c34738509d4975dd992ee3a11f
Joe Moloney <joe.moloney496@gmail.com>
2026-06-16 17:35:54 +0100

    swap to interface

--- src/main/java/joe/aurora/services/validators/TransferValidator.java
+++ src/main/java/joe/aurora/services/validators/TransferValidator.java
@@ -3,11 +3,8 @@ package joe.aurora.services.validators;
 import java.math.BigDecimal;
 
 import joe.aurora.domains.wallet.Wallet;
-import joe.aurora.exceptions.InsufficientFundsException;
 
-public class TransferValidator {
-    public void validate(Wallet wallet, BigDecimal amount) {
-        if(amount.compareTo(BigDecimal.ZERO) < 0)
-            throw new InsufficientFundsException("Insufficient Funds");
-    } 
+public interface TransferValidator {
+    public void validate(Wallet wallet, BigDecimal amount);
+
 }
