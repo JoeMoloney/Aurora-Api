db3572b87f63765d67801f66430192495d7d67b4
Joe Moloney <joe.moloney496@gmail.com>
2026-06-16 18:49:08 +0100

    add constructor + accessor methods

--- src/main/java/joe/aurora/dtos/TransferRequestDTO.java
+++ src/main/java/joe/aurora/dtos/TransferRequestDTO.java
@@ -9,4 +9,21 @@ public class TransferRequestDTO {
     private Long sourceWalletId;
     private Long destinationWalletId;
     private BigDecimal amount;
+
+    public TransferRequestDTO() {};
+    public TransferRequestDTO(Long sourceWalletId, Long destinationWalletId, BigDecimal amount) {
+        this.sourceWalletId = sourceWalletId;
+        this.destinationWalletId = destinationWalletId;
+        this.amount = amount;
+    }
+
+    public Long getSourceWalletId() {
+        return this.sourceWalletId;
+    }
+    public Long getDestinationWalletId() {
+        return this.destinationWalletId;
+    }
+    public BigDecimal getAmount() {
+        return this.amount;
+    }
 }
